a30503bc65b97eff2f21af46aef5deb2860151d6
BechirBerraies <136343630+BechirBerraies@users.noreply.github.com>
2024-05-19 18:28:07 +0200

    spring running

--- Song/songs/src/main/java/com/example/songs/SongsApplication.java
+++ Song/songs/src/main/java/com/example/songs/SongsApplication.java
@@ -2,8 +2,15 @@ package com.example.songs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan("java.com.example.songs.model")
+@EnableJpaRepositories("java.com.example.songs.repository")
+@ComponentScan("com.example.songs.repository")
 public class SongsApplication {
 
 	public static void main(String[] args) {
--- Song/songs/src/main/java/com/example/songs/repository/SongRepository.java
+++ Song/songs/src/main/java/com/example/songs/repository/SongRepository.java
@@ -1,5 +1,6 @@
 package com.example.songs.repository;
 
+
 import com.example.songs.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
